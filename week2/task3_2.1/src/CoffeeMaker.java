9d12bcedbda84e0066fe45932838bd2030b0f387
Trang Vu <Trang.Vu3@metropolia.fi>
2023-11-02 23:54:08 +0200

    add task 3 part 2.1

--- week2/task3_2.1/src/CoffeeMaker.java
+++ week2/task3_2.1/src/CoffeeMaker.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class CoffeeMaker {
+public class CoffeeMaker {
+    private boolean on;
+    private int coffeeAmount;
+    private int coffeeType;
+    public CoffeeMaker() {
+        this.on = false;
+        this.coffeeType = 1;
+        this.coffeeAmount = 10;
+    }
+    public void pressOnOff() {
+        this.on = !on;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public int getCoffeeType() {
+        return coffeeType;
+    }
+
+    public void selectCoffeeType(int type) {
+        if (on && (type == 1 || type == 2)) {
+            coffeeType = type;
+        }
+    }
+    public int getAmount() {
+        return coffeeAmount;
+    }
+
+    public void selectAmount(int amount) {
+        if (on && amount >= 10 && amount <= 80) {
+            coffeeAmount = amount;
+        }
+    }
 }
--- week2/task3_2.1/src/CoffeeProgram.java
+++ week2/task3_2.1/src/CoffeeProgram.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class CoffeeProgram {
+import java.util.Scanner;
+
+public class CoffeeProgram {
+    public static void main(String[] args) {
+        CoffeeMaker myCoffeeMaker = new CoffeeMaker();
+        Scanner scanner = new Scanner(System.in);
+
+        if (!myCoffeeMaker.isOn()) {
+            myCoffeeMaker.pressOnOff();
+        }
+
+        int coffeeType = getCoffeeTypeFromUser(scanner);
+        if (coffeeType != -1) {
+            int coffeeAmount = getCoffeeAmountFromUser(scanner);
+
+            if (coffeeAmount != -1) {
+                prepareCoffee(myCoffeeMaker, coffeeType, coffeeAmount);
+            } else {
+                System.out.println("Invalid coffee amount.");
+            }
+        } else {
+            System.out.println("Invalid coffee type. Please choose 1 for normal coffee or 2 for espresso coffee.");
+        }
+
+        myCoffeeMaker.pressOnOff();
+        scanner.close();
+    }
+
+    private static int getCoffeeTypeFromUser(Scanner scanner) {
+        System.out.println("Press 1 to choose normal coffee, press 2 to choose espresso coffee:");
+        int coffeeType = scanner.nextInt();
+        if (coffeeType == 1 || coffeeType == 2) {
+            System.out.println("You chose " + (coffeeType == 1 ? "normal" : "espresso") + " coffee.");
+            return coffeeType;
+        }
+        return -1;
+    }
+
+    private static int getCoffeeAmountFromUser(Scanner scanner) {
+        System.out.println("Please choose coffee amount (10-80 ml): ");
+        int coffeeAmount = scanner.nextInt();
+        if (coffeeAmount >= 10 && coffeeAmount <= 80) {
+            return coffeeAmount;
+        }
+        return -1;
+    }
+
+    private static void prepareCoffee(CoffeeMaker coffeeMaker, int coffeeType, int coffeeAmount) {
+        boolean full = false;
+        if (!full && coffeeAmount <= 80) {
+            for (int j = 10; j <= coffeeAmount; j += 10) {
+                System.out.println("Loading: " + j + " ml");
+            }
+            full = true;
+            System.out.println("Your coffee is ready");
+            coffeeMaker.selectCoffeeType(coffeeType);
+            coffeeMaker.selectAmount(coffeeAmount);
+        }
+    }
 }
