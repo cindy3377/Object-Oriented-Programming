136331e3ae2a2bba8f2f742f8392777bd4a670fc
Trang Vu <Trang.Vu3@metropolia.fi>
2023-11-08 09:54:00 +0200

    add task 4 Programming Project 1.1 Introduction to Simulation

--- Programming_Project/_1.1_Introduction_to_Simulation/task4/src/LinkedList.java
+++ Programming_Project/_1.1_Introduction_to_Simulation/task4/src/LinkedList.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class LinkedList {
+
+import java.util.LinkedList;
+
+class Customer {
+    private static int nextId = 1 ;
+    private int id;
+    private long startTime;
+    private long endTime;
+
+    public Customer () {
+        this.id = nextId++;
+        this.startTime = System.currentTimeMillis();
+        this.endTime = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+    public long getTimeSpentInQueue() {
+        return endTime - startTime;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Customer> customerQueue = new LinkedList<>();
+        for (int i = 1; i <= 3; i++) {
+            Customer customer = new Customer();
+            customerQueue.addFirst(customer);
+            System.out.println("Customer " + customer.getId() + " joined the queue.");
+        }
+        while (!customerQueue.isEmpty()) {
+            Customer removedCustomer = customerQueue.removeLast();
+            removedCustomer.setEndTime(System.nanoTime());
+            long timeSpentInQueue = removedCustomer.getTimeSpentInQueue();
+            System.out.println("Customer " + removedCustomer.getId() + " was removed from the queue and spent " + timeSpentInQueue + " nanoseconds in the queue.");
+        }
+    }
 }
+
+
