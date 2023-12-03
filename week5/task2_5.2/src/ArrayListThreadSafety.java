d49edec7b11eea01bb551cd0e5015c9d9aacc22b
Trang Vu <Trang.Vu3@metropolia.fi>
2023-12-03 15:44:22 +0200

    add task 2 part 5.2

--- week5/task2_5.2/src/ArrayListThreadSafety.java
+++ week5/task2_5.2/src/ArrayListThreadSafety.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ArrayListThreadSafety {
+public class ArrayListThreadSafety {
+    public static void main(String[] args) {
+        SafeArrayList<String> safeArrayList = new SafeArrayList<>();
+        Thread[] threads = new Thread[3];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new ArrayListTester(safeArrayList));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Final ArrayList Size: " + safeArrayList.getSize());
+    }
 }
--- week5/task2_5.2/src/SafeArrayList.java
+++ week5/task2_5.2/src/SafeArrayList.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class SafeArrayList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class SafeArrayList<E> {
+    private List<E> list = Collections.synchronizedList(new ArrayList<>());
+
+    public void addElement(E element) {
+        list.add(element);
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public E removeElement(int index) {
+        if (index >= 0 && index < list.size()) {
+            return list.remove(index);
+        } else {
+            return null;
+        }
+    }
 }
+
+class ArrayListTester implements Runnable {
+    private static int threadIDCounter = 1;
+    private int threadID;
+    private SafeArrayList<String> safeArrayList;
+
+    public ArrayListTester(SafeArrayList<String> safeArrayList) {
+        this.threadID = threadIDCounter++;
+        this.safeArrayList = safeArrayList;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            safeArrayList.addElement("Thread " + threadID + ": Element " + i);
+        }
+    }
+}
+
