34bfaaf3814f237dbad5e17e5cd582a77344ee70
Trang Vu <Trang.Vu3@metropolia.fi>
2023-12-05 14:00:26 +0200

    git add task 2 part 5.3

--- week5/task2_5.3/src/StringManipulatorTest.java
+++ week5/task2_5.3/src/StringManipulatorTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class StringManipulatorTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringManipulatorTest {
+
+    @Test
+    void testConcatenate() {
+        StringManipulator manipulator = new StringManipulator();
+        assertEquals("HelloWorld", manipulator.concatenate("Hello", "World"));
+    }
+
+    @Test
+    void testFindLength() {
+        StringManipulator manipulator = new StringManipulator();
+        assertEquals(5, manipulator.findLength("Hello"));
+    }
+
+    @Test
+    void testConvertToUpperCase() {
+        StringManipulator manipulator = new StringManipulator();
+        assertEquals("HELLO", manipulator.convertToUpperCase("Hello"));
+    }
+
+    @Test
+    void testConvertToLowerCase() {
+        StringManipulator manipulator = new StringManipulator();
+        assertEquals("hello", manipulator.convertToLowerCase("Hello"));
+    }
+
+    @Test
+    void testContainsSubstring() {
+        StringManipulator manipulator = new StringManipulator();
+        assertTrue(manipulator.containsSubstring("Hello, World!", "World"));
+        assertFalse(manipulator.containsSubstring("Hello, World!", "Goodbye"));
+    }
 }
